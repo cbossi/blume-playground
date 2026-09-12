77528d969f5130fce3c844d2d84180ad55144d88
Christian Bosshard <christian.bosshard@ergon.ch>
2026-09-12 15:57:22 +0200

    Blume init.

---
+++ blume.config.ts
@@ -0,0 +1,9 @@
+import { defineConfig } from "blume";
+
+export default defineConfig({
+  title: "Blume Playground",
+  description: "Documentation powered by Blume.",
+  content: {
+    root: "content",
+  },
+});
